8ee04b8a54a7479bc457025478ff18e26a3ac63c
Eduardo de la Cruz <eduardodelacruz@mac-mini-de-eduardo-de-la-cruz.local>
2017-09-22 11:02:07 -0400

    Button distribution changed

--- AdosFirebaseSocialLogin/AdosLoginViewController.swift
+++ AdosFirebaseSocialLogin/AdosLoginViewController.swift
@@ -12,7 +12,7 @@ import Firebase
 import GoogleSignIn
 import TwitterKit
 
-class AdosLoginViewController: UIViewController, FBSDKLoginButtonDelegate, GIDSignInUIDelegate, GIDSignInDelegate
+class AdosLoginViewController: UIViewController, GIDSignInUIDelegate, GIDSignInDelegate
 {
     // MARK: - Global Variables
     
@@ -28,7 +28,7 @@ class AdosLoginViewController: UIViewController, FBSDKLoginButtonDelegate, GIDSi
     @IBOutlet var loginButton: UIButton!
     @IBOutlet var passwordTextField: UITextField!
     @IBOutlet var emailTextField: UITextField!
-    @IBOutlet var customFBButton: FBSDKLoginButton!
+    @IBOutlet var customFBButton: UIButton!
     @IBOutlet var signUpButton: UIButton!
     @IBOutlet var customGoogleButton: UIButton!
     @IBOutlet var customTwitterButton: UIButton!
@@ -39,10 +39,7 @@ class AdosLoginViewController: UIViewController, FBSDKLoginButtonDelegate, GIDSi
     {
         super.viewDidLoad()
         
-        setupFacebookButtons()
-        setupLoginButton()
-        setupTextFields()
-        setupGoogleButton()
+        googleDelegateConfig()
     }
     
     override func viewWillAppear(_ animated: Bool)
@@ -50,85 +47,19 @@ class AdosLoginViewController: UIViewController, FBSDKLoginButtonDelegate, GIDSi
         super.viewWillAppear(animated)
         
         self.navigationController?.setNavigationBarHidden(true, animated: animated)
-        
-        if GIDSignIn.sharedInstance().hasAuthInKeychain()
-        {
-            customGoogleButton.setImage(#imageLiteral(resourceName: "Sign_Out"), for: .normal)
-        }
-        else
-        {
-            customGoogleButton.setImage(#imageLiteral(resourceName: "sign_In"), for: .normal)
-        }
-    }
-    
-    override func viewDidAppear(_ animated: Bool)
-    {
-        super .viewDidAppear(animated)
-        
-        self.loginButtonHeight = loginButton.frame.height
-        self.loginButton.layer.cornerRadius = self.loginButtonHeight / 2.0
-    }
-    
-    // MARK: - Twitter Btton Action
-    
-    @IBAction func customTwitterButtonPressed(_ sender: TWTRLogInButton)
-    {
-        Twitter.sharedInstance().logIn(completion: { (session, error) in
-            if let err = error
-            {
-                print("Failed to log in via Twitter", err)
-            }
-            
-            guard let token = session?.authToken else { return }
-            guard let secret = session?.authTokenSecret else { return }
-            
-            let credential = TwitterAuthProvider.credential(withToken: token, secret: secret)
-            
-            Auth.auth().signIn(with: credential, completion: { (user, error) in
-                if let err = error
-                {
-                    print("Failed to login to Firebase with Twitter: ", err)
-                    return
-                }
-                
-                print("Successfully created a Firebase-Twitter user: ", user?.uid ?? "")
-            })
-            
-            self.token = (session?.authToken)!
-            self.name = (session?.userName)!
-            self.email = "Not Provided"
-            self.imageUrl = "No Picture"
-            
-            self.performSegue(withIdentifier: "goToProfileView", sender: nil)
-        })
-    }
-    
-    // MARK: - Google Sign in Button
-
-    fileprivate func setupGoogleButton()
-    {
-        customGoogleButton.contentMode = .center
-        customGoogleButton.imageView?.contentMode = .scaleAspectFit
-        
-        GIDSignIn.sharedInstance().delegate = self
-        GIDSignIn.sharedInstance().uiDelegate = self
-        GIDSignIn.sharedInstance().clientID = FirebaseApp.app()?.options.clientID
-        
-        customGoogleButton.addTarget(self, action: #selector (handleCustomGoogleSign), for: .touchUpInside)
-    }
-    
-    // MARK: - Custom Facebook button config
-    
-    fileprivate func setupFacebookButtons()
-    {
-        customFBButton.delegate = self
-        customFBButton.readPermissions = ["email", "public_profile"]
+        setupLoginButton()
+        setupFacebookButtons()
+        setupTextFields()
+        setupGoogleButton()
+        setupTwitterButton()
     }
     
     // MARK: - Login Button config
     
     fileprivate func setupLoginButton()
     {
+        self.loginButtonHeight = loginButton.frame.height
+        self.loginButton.layer.cornerRadius = self.loginButtonHeight / 2.0
         self.loginButton.layer.borderColor = UIColor.white.cgColor
         self.loginButton.layer.borderWidth = 1
     }
@@ -148,24 +79,25 @@ class AdosLoginViewController: UIViewController, FBSDKLoginButtonDelegate, GIDSi
         }
     }
     
-    // MARK: - FB Button functions
-    
-    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!)
+    func setupFacebookButtons()
     {
-        FBSDKLoginManager().logOut()
-        
-        print("Did log out of facebook")
+        let customFBButtonHeight = customFBButton.frame.height
+        self.customFBButton.layer.cornerRadius = customFBButtonHeight / 2.0
     }
     
-    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!)
+    // MARK: - Custom Facebook button action
+    
+    @IBAction func customFBButtonPressed(_ sender: UIButton)
     {
-        if error != nil
-        {
-            print(error.localizedDescription)
-            return
+        FBSDKLoginManager().logIn(withReadPermissions: ["email", "public_profile"], from: self) { (result, error) in
+            if let err = error
+            {
+                print("Custom FB Login failed", err)
+                return
+            }
+            
+            self.facebookLoginAction()
         }
-        
-        self.facebookLoginAction()
     }
     
     // MARK: - Facebook Login Action
@@ -235,6 +167,27 @@ class AdosLoginViewController: UIViewController, FBSDKLoginButtonDelegate, GIDSi
         } // Brings your user profile
     }
     
+    // MARK: - Google Button Setup
+    
+    fileprivate func setupGoogleButton()
+    {
+        let customGoogleHeight = customGoogleButton.frame.height
+        self.customGoogleButton.layer.cornerRadius = customGoogleHeight / 2.0
+    }
+    
+    // MARK: - Google Delegate Config
+
+    fileprivate func googleDelegateConfig()
+    {
+        GIDSignIn.sharedInstance().delegate = self
+        GIDSignIn.sharedInstance().uiDelegate = self
+        GIDSignIn.sharedInstance().clientID = FirebaseApp.app()?.options.clientID
+        
+        customGoogleButton.addTarget(self, action: #selector (handleCustomGoogleSign), for: .touchUpInside)
+    }
+    
+    // MARK: - Google Sign In Action
+    
     func googleButtonAction()
     {
         self.email = GIDSignIn.sharedInstance().currentUser.profile.email
@@ -246,21 +199,21 @@ class AdosLoginViewController: UIViewController, FBSDKLoginButtonDelegate, GIDSi
         self.performSegue(withIdentifier: "goToProfileView", sender: nil)
     }
     
-    // MARK: - Google Button Login Action
+    // MARK: - Google Button Login / LogOut
     
     func handleCustomGoogleSign()
     {
         if GIDSignIn.sharedInstance().hasAuthInKeychain()
         {
             GIDSignIn.sharedInstance().signOut()
-            customGoogleButton.setImage(#imageLiteral(resourceName: "sign_In"), for: .normal)
         }
         else
         {
             GIDSignIn.sharedInstance().signIn()
-            
         }
     }
+    
+    // MARK: - Google Delegate Methods
     
     public func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!)
     {    
@@ -300,6 +253,48 @@ class AdosLoginViewController: UIViewController, FBSDKLoginButtonDelegate, GIDSi
         {
             print("Successfully logged out from Google")
         }
+    }
+    
+    // MARK: - Twitter Button Setup
+    
+    func setupTwitterButton()
+    {
+        let customTwitterHeight = customTwitterButton.frame.height
+        self.customTwitterButton.layer.cornerRadius = customTwitterHeight / 2.0
+    }
+    
+    // MARK: - Twitter Button Action
+    
+    @IBAction func customTwitterButtonPressed(_ sender: TWTRLogInButton)
+    {
+        Twitter.sharedInstance().logIn(completion: { (session, error) in
+            if let err = error
+            {
+                print("Failed to log in via Twitter", err)
+            }
+            
+            guard let token = session?.authToken else { return }
+            guard let secret = session?.authTokenSecret else { return }
+            
+            let credential = TwitterAuthProvider.credential(withToken: token, secret: secret)
+            
+            Auth.auth().signIn(with: credential, completion: { (user, error) in
+                if let err = error
+                {
+                    print("Failed to login to Firebase with Twitter: ", err)
+                    return
+                }
+                
+                print("Successfully created a Firebase-Twitter user: ", user?.uid ?? "")
+            })
+            
+            self.token = (session?.authToken)!
+            self.name = (session?.userName)!
+            self.email = "Not Provided"
+            self.imageUrl = "No Picture"
+            
+            self.performSegue(withIdentifier: "goToProfileView", sender: nil)
+        })
     }
     
     // MARK: - Navigation
